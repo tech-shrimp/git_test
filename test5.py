2f2854dcc921c64f7853a819a08ad120dfc05b3d
tech-shrimp <techshrimp@163.com>
2024-08-05 18:42:18 +0800

    1

--- test5.py
+++ test5.py
@@ -1,3 +1,4 @@
-mylist = ['blue', 'orange', 'green']
-mapped_dict = dict(zip(itr, map(fn, itr)))
-print(mapped_dict)
+keys = ['name', 'age', 'city']
+values = ['Alice', 25, 'New York']
+dictionary = dict(zip(keys, values))
+print(dictionary)
