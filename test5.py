901cfcf4d6a8786b8e879b27efc417ac65011608
tech-shrimp <techshrimp@163.com>
2024-08-06 09:43:26 +0800

    commit3

--- test5.py
+++ test5.py
@@ -2,6 +2,6 @@ keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New York']
 dictionary = dict(zip(keys, values))
 print(dictionary)
-print("第一部分")
+print("第1部分")
 print("第二部分")
 print("第三部分")
