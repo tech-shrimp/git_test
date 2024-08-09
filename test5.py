c1b7569203ed0cf45fc691fb1b8d0a6a8ce05d99
tech-shrimp <techshrimp@163.com>
2024-08-09 17:06:14 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,7 +1,8 @@
 keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New York']
 dictionary = dict(zip(keys, values))
-print(dictionary)
+print("what is amend")
+print("what is amend")
 print("第1部分")
 print("第2部分")
 print("第3部分")
