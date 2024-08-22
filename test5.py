bb361638e874667263edd5a826111c8de463f54e
tech-shrimp <techshrimp@163.com>
2024-08-22 16:07:10 +0800

    1

--- test5.py
+++ test5.py
@@ -2,7 +2,3 @@ keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New York']
 dictionary = dict(zip(keys, values))
 print(dictionary)
-print("第1部分")
-print("第2部分")
-print("第4部分")
-print("main分支修改")
